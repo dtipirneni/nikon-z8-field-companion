0b3dc7313e738ac6e3e26d3c53b30839a5769786
dtipirneni <dtipirneni@gmail.com>
2026-07-11 20:43:44 -0700

    Initial Nikon Z8 Field Companion

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='z8-safari-pwa-v5';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./assets/exposure-triangle-purple.png','./assets/photography-cheat-sheet.jpeg','./assets/exposure-triangle-green.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./index.html'))))});
